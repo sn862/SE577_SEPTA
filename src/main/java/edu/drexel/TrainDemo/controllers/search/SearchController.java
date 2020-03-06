fae87a500031a7110ae9c74027b91a8575189b99
srini <sn862@drexel.edu>
2020-03-06 00:02:40 -0500

    main page UI work

--- src/main/java/edu/drexel/TrainDemo/controllers/search/SearchController.java
+++ src/main/java/edu/drexel/TrainDemo/controllers/search/SearchController.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.drexel.TrainDemo.entities.itinerary.Stop;
 import edu.drexel.TrainDemo.model.Itinerary.Journey;
@@ -21,8 +25,10 @@ public class SearchController {
 	private SearchService searchService;
 
 	@GetMapping("/getStops/{searchString}")
-	public List<Stop> getStops(@PathVariable("searchString") String searchString) {
-		return searchService.getStops(searchString);
+	public String getStops(@PathVariable("searchString") String searchString) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(searchService.getStops(searchString));
+		return json;
 	}
 
 	/*
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,31 +1,67 @@
-$.ajaxSetup({
-    beforeSend: function (xhr, settings) {
-        if (settings.type == 'POST' || settings.type == 'PUT'
-            || settings.type == 'DELETE') {
-            if (!(/^http:.*/.test(settings.url) || /^https:.*/
-                .test(settings.url))) {
-                // Only send the token to relative URLs i.e. locally.
-                xhr.setRequestHeader("X-XSRF-TOKEN",
-                    Cookies.get('XSRF-TOKEN'));
-            }
-        }
-    }
+$
+		.ajaxSetup({
+			beforeSend : function(xhr, settings) {
+				if (settings.type == 'POST' || settings.type == 'PUT'
+						|| settings.type == 'DELETE') {
+					if (!(/^http:.*/.test(settings.url) || /^https:.*/
+							.test(settings.url))) {
+						// Only send the token to relative URLs i.e. locally.
+						xhr.setRequestHeader("X-XSRF-TOKEN", Cookies
+								.get('XSRF-TOKEN'));
+					}
+				}
+			}
+		});
+
+$.get("/user", function(data) {
+	$("#profile-name").html(data.name);
+	$(".unauthenticated").hide();
+	$(".authenticated").show();
+}).fail(function() {
+	$(".unauthenticated").show();
+	$(".authenticated").hide();
 });
 
-$.get("/user", function (data) {
-    $("#profile-name").html(data.name);
-    $(".unauthenticated").hide();
-    $(".authenticated").show();
-}).fail(function () {
-    $(".unauthenticated").show();
-    $(".authenticated").hide();
-});
-
-var logout = function () {
-    $.post("/logout", function () {
-        $("#profile-name").html("");
-        $(".unauthenticated").show();
-        $(".authenticated").hide();
-    })
-    return true;
+var logout = function() {
+	$.post("/logout", function() {
+		$("#profile-name").html("");
+		$(".unauthenticated").show();
+		$(".authenticated").hide();
+	})
+	return true;
 }
+function onload() {
+	var x = document.getElementById("oneway").value;
+	if (x == "OneWay") {
+		document.getElementById("arrivalDate").disabled = true;
+	}
+}
+
+function enableReturnDate() {
+	document.getElementById("arrivalDate").disabled = false;
+}
+
+function disableReturnDate() {
+	document.getElementById("arrivalDate").disabled = true;
+}
+
+
+function oneverykey(val) {
+	//alert(val);
+	var result=[];
+	var url1 = "http://localhost:8080/search/getStops/"+val;
+	 $.ajax({
+	        url: url1
+	    }).then(function(data) {
+	    	 var myArr = JSON.parse(data);
+	    	 for (i = 0; i < myArr.length; i++) {
+	    		 var x= myArr[i].name+" ("+myArr[i].id+")";
+	    		 result.push(x);
+	    		}
+	    	alert(result); 
+	    });
+	 
+}
+
+
+
