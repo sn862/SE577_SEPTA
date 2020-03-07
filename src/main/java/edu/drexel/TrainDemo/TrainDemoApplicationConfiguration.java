35fa4cbc71da85887b56254fc24caf188bbf1803
srini <sn862@drexel.edu>
2020-03-07 12:31:46 -0500

    updated html pages path with display items

--- src/main/java/edu/drexel/TrainDemo/TrainDemoApplicationConfiguration.java
+++ src/main/java/edu/drexel/TrainDemo/TrainDemoApplicationConfiguration.java
@@ -1,9 +1,27 @@
 package edu.drexel.TrainDemo;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@EnableWebMvc
 @ComponentScan({"edu.drexel.TrainDemo"})
-public class TrainDemoApplicationConfiguration {
+public class TrainDemoApplicationConfiguration extends WebMvcConfigurerAdapter{
+	@Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        registry.viewResolver(resolver);
+    }
 
+	    @Override
+	    public void configureDefaultServletHandling(
+	            DefaultServletHandlerConfigurer configurer) {
+	        configurer.enable();
+	    }
+	    
+	   
 }
