cb8a2467c4c3ff12234d050d5cf67e74e309765c
srini <sn862@drexel.edu>
2020-03-07 23:30:18 -0500

    search funtionality with display page

---
+++ src/main/java/edu/drexel/TrainDemo/model/search/SearchModel.java
@@ -0,0 +1,55 @@
+package edu.drexel.TrainDemo.model.search;
+
+import java.util.Date;
+
+public class SearchModel {
+
+	private String fromStn;
+	private String toStn;
+	private String departureDate;
+	private String arrivalDate;
+	private int numberOfTickets;
+	private String tripType;
+	public String getFromStn() {
+		return fromStn;
+	}
+	public void setFromStn(String fromStn) {
+		this.fromStn = fromStn;
+	}
+	public String getToStn() {
+		return toStn;
+	}
+	public void setToStn(String toStn) {
+		this.toStn = toStn;
+	}
+	
+	public int getNumberOfTickets() {
+		return numberOfTickets;
+	}
+	public void setNumberOfTickets(int numberOfTickets) {
+		this.numberOfTickets = numberOfTickets;
+	}
+	
+	public String getTripType() {
+		return tripType;
+	}
+	public void setTripType(String tripType) {
+		this.tripType = tripType;
+	}
+	public String getDepartureDate() {
+		return departureDate;
+	}
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+	public String getArrivalDate() {
+		return arrivalDate;
+	}
+	public void setArrivalDate(String arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+	
+	
+	
+	
+}
